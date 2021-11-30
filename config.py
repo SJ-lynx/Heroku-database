dcd0f9602855bac08165da4eee92795d1bcff1d8
alimkimbek <88457845+alimkimbek@users.noreply.github.com>
2021-11-30 17:46:47 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "2020615072:AAGQNWHe9YBc9k6qegBLJYBgWqOli9B3vJw"
+APP_URL = "https://postslqsj.herokuapp.com/" + BOT_TOKEN
+DB_URI = "postgres://hhntmoinyskxrg:b366839d824f78b712c1b69d2fc980680f2847e9b6dc8cf907131c3897180241@ec2-54-74-60-70.eu-west-1.compute.amazonaws.com:5432/d598hppgrfqj6a"
